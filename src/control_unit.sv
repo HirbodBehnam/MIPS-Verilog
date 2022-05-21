f45d28b27380a2753cf99cb0cd90ee60b295ffa2
Beny-Maleki <benyaminmaleki81@gmail.com>
2022-05-21 19:42:15 +0430

    Creating 'control_unit.sv' module

---
+++ src/control_unit.sv
@@ -0,0 +1,16 @@
+module CU(
+    input wire [5:0] opcode,
+    output reg RegDest,
+    output reg Jump,
+    output reg Branch,
+    output reg MemToReg,
+    output reg [5:0] ALUOp,
+    output reg MemWrite,
+    output reg ALUsrc,
+    output reg RegWrite,
+    output reg jalCtrl,
+    output reg jrCtrl
+);
+
+
+endmodule
