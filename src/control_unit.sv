d55c4520cbc734ad62efeee0aae631b2b7e68daa
Beny-Maleki <benyaminmaleki81@gmail.com>
2022-05-22 00:52:04 +0430

    Completing I type in CU and another 'include' at top of file

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -1,4 +1,5 @@
 `include "src/control_unit_macros.sv"
+`include "src/alu_opts.sv"
 
 module CU(
     input wire [5:0] opcode,
@@ -44,47 +45,69 @@ module CU(
         default:
             case (opcode)
                 ADDi: begin
-
+                    {ALUsrc, PCorMemALU, RegWrite} = 3'b111;
+                    {RegDest, MemToReg, Link, MemRead, MemWrite, Jump} = 6'b000000;
+                    {ALUOp} = ALU_ADD;
                 end
 
-                ADDiu: begin 
-
+                ADDiu: begin // similar to 'ADDi' in control signals
+                    {ALUsrc, PCorMemALU, RegWrite} = 3'b111;
+                    {RegDest, MemToReg, Link, MemRead, MemWrite, Jump} = 6'b000000;
+                    {ALUOp} = ALU_ADD;
                 end
 
                 ANDi: begin
-
+                    {ALUsrc, PCorMemALU, RegWrite} = 3'b111;
+                    {RegDest, MemToReg, Link, MemRead, MemWrite, Jump} = 6'b000000;
+                    {ALUOp} = ALU_AND;
                 end
 
                 XORi: begin 
-
+                    {ALUsrc, PCorMemALU, RegWrite} = 3'b111;
+                    {RegDest, MemToReg, Link, MemRead, MemWrite, Jump} = 6'b000000;
+                    {ALUOp} = ALU_XOR;
                 end
 
                 ORi: begin
-
+                    {ALUsrc, PCorMemALU, RegWrite} = 3'b111;
+                    {RegDest, MemToReg, Link, MemRead, MemWrite, Jump} = 6'b000000;
+                    {ALUOp} = ALU_OR;
                 end
 
                 BEQ: begin
-
+                    {Branch} = 1'b1;
+                    {ALUsrc, RegWrite, MemRead, MemWrite, Jump} = 5'b00000;
+                    {ALUOp} = ALU_COMP_NEQ; // "out" in ALU is 0 then "zero" flag is 1
                 end 
 
                 BNE: begin 
-
+                    {Branch} = 1'b1;
+                    {ALUsrc, RegWrite, MemRead, MemWrite, Jump} = 5'b00000;
+                    {ALUOp} = ALU_COMP_EQ; // "out" in ALU is 0 then "zero" flag is 1
                 end 
 
                 BLEZ: begin
-
+                    {Branch} = 1'b1;
+                    {ALUsrc, RegWrite, MemRead, MemWrite, Jump} = 5'b00000;
+                    {ALUOp} = ALU_COMP_GT; // "out" in ALU is 0 then "zero" flag is 1
                 end
 
                 BGTZ: begin
-
+                    {Branch} = 1'b1;
+                    {ALUsrc, RegWrite, MemRead, MemWrite, Jump} = 5'b00000;
+                    {ALUOp} = ALU_COMP_LT; // "out" in ALU is 0 then "zero" flag is 1
                 end
 
                 LW: begin
-
+                    {ALUsrc, PCorMemALU, RegWrite, MemRead, MemToReg} = 5'b11111;
+                    {Jump, Link, RegDest, MemWrite} = 4'b0000;
+                    {ALUOp} = ALU_ADD;
                 end
 
                 SW: begin
-
+                    {ALUsrc, MemWrite} = 2'b11;
+                    {Jump, RegWrite, MemRead, RegDest} = 5'b00000;
+                    {ALUOp} = ALU_ADD;
                 end
 
                 LB: begin
