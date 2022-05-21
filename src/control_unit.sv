094d172af48a7058e8ff20effcd2b36eecf42b89
Beny-Maleki <benyaminmaleki81@gmail.com>
2022-05-21 23:39:06 +0430

    Completing J type in CU and some modifications in module signutare

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -4,11 +4,15 @@ module CU(
     input wire [5:0] opcode,
     input wire [5:0] func,
     output reg RegDest,
+    output reg Link,
     output reg Jump,
+    output reg JumpReg,
     output reg Branch,
     output reg MemToReg,
+    output reg PCorMemALU,
     output reg [5:0] ALUOp,
     output reg MemWrite,
+    output reg MemRead,
     output reg ALUsrc,
     output reg RegWrite,
     output reg jalCtrl,
@@ -25,15 +29,71 @@ module CU(
         // J type opts
         J_TYPE:
             case (opcode)
-                : 
-                default: 
+                J: begin
+                    Jump = 1'b1;
+                    {RegWrite, MemToReg, MemWrite, MemRead} = 4'b0000;
+                end
+
+                JAL: begin
+                    {RegWrite, Link, Jump} = 3'b111;
+                    {PCorMemALU, MemRead, MemWrite, JumpReg} = 4'b0000;
+                end
             endcase
 
         // I type opts
         default:
             case (opcode)
-                : 
-                default: 
+                ADDi: begin
+
+                end
+
+                ADDiu: begin 
+
+                end
+
+                ANDi: begin
+
+                end
+
+                XORi: begin 
+
+                end
+
+                ORi: begin
+
+                end
+
+                BEQ: begin
+
+                end 
+
+                BNE: begin 
+
+                end 
+
+                BLEZ: begin
+
+                end
+
+                BGTZ: begin
+
+                end
+
+                LW: begin
+
+                end
+
+                SW: begin
+
+                end
+
+                LB: begin
+
+                end
+
+                SB: begin 
+
+                end
             endcase
 
     endcase
