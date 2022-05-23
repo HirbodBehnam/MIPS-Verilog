1a17aefaf7f027cbe2e376e75ed84ec390cf400b
soheilnazari2001 <s.nazari80820@gmail.com>
2022-05-23 19:48:32 +0430

    some structural changes in cu

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -21,6 +21,7 @@ module CU(
 );
 
     always @(*) begin
+    {ALUsrc, Jump, Branch, MemRead, MemToReg, MemWrite, RegDest, RegWrite, Link, SignExtend} = 10'b0000000000;
 	if(~rst_b)
 	begin
 		Halted = 0;
@@ -33,87 +34,70 @@ module CU(
             case (func)
                 `XOR:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_XOR;
                     end
                 `SLL:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump, Branch, MemRead, MemToReg, MemWrite}=6'b000000;
                     {ALUOp} = `ALU_UNSIGNED_SHIFT_LEFT_SH_AMOUNT;
                     end
                 `SLLV:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_UNSIGNED_SHIFT_LEFT;
                     end
                 `SRL:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump, Branch, MemRead, MemToReg, MemWrite}=6'b000000;
                     {ALUOp} = `ALU_UNSIGNED_SHIFT_RIGHT_SH_AMOUNT;
                     end
                 `SUB:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_SUB;
                     end
                 `SRLV:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_UNSIGNED_SHIFT_RIGHT;
                     end
                 `SLT:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_COMP_LT;
                     end
                 `SUBU:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_SUB;
                     end
                 `OR:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_OR;
                     end
                 `NOR:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_NOR;
                     end
                 `ADDU:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_ADD;
                     end
                 `MULT:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_MULT;
                     end
                 `DIV:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_DIV;
                     end
                 `AND:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_AND;
                     end
                 `ADD:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_ADD;
                     end
                 `SRA:begin
                     {RegDest,Link,RegWrite}=3'b111;
-                    {ALUsrc, Jump,Branch,MemRead,MemToReg,MemWrite}=6'b000000;
                     {ALUOp} = `ALU_SIGNED_SHIFT_RIGHT_SH_AMOUNT;
                     end
                 `JR:begin
                     {Jump, JumpReg} = 2'b11;
-                    {RegWrite, MemRead, MemWrite} = 3'b000;
                     end
                 default:begin
                     $display("UNKNOWN FUNC: %b", func);
@@ -126,12 +110,10 @@ module CU(
             case (opcode)
                 `J: begin
                     Jump = 1'b1;
-                    {RegWrite, MemToReg, MemWrite, MemRead} = 4'b0000;
                 end
 
                 `JAL: begin
                     {RegWrite, Jump} = 2'b11;
-                    {Link, MemRead, MemWrite, JumpReg} = 4'b0000;
                 end
 		default:begin
 		end
@@ -142,93 +124,77 @@ module CU(
             case (opcode)
                 `ADDi: begin
                     {ALUsrc, Link, RegWrite} = 3'b111;
-                    {RegDest, MemToReg, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_ADD;
-		    SignExtend = 1;
+		            SignExtend = 1;
                 end
 
                 `ADDiu: begin // similar to 'ADDi' in control signals
                     {ALUsrc, Link, RegWrite} = 3'b111;
-                    {RegDest, MemToReg, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_ADD;
-		    SignExtend = 0;
                 end
 
                 `ANDi: begin	// DO NOT EXTEND SIGN
                     {ALUsrc, Link, RegWrite} = 3'b111;
-                    {RegDest, MemToReg, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_AND;
-		    SignExtend = 0;
                 end
 
                 `XORi: begin 	// DO NOT EXTEND SIGN
                     {ALUsrc, Link, RegWrite} = 3'b111;
-                    {RegDest, MemToReg, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_XOR;
-		    SignExtend = 0;
                 end
 
                 `ORi: begin	// DO NOT EXTEND SIGN
                     {ALUsrc, Link, RegWrite} = 3'b111;
-                    {RegDest, MemToReg, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_OR;
-		    SignExtend = 0;
+		      
                 end
 
                 `BEQ: begin
                     {Branch} = 1'b1;
-                    {ALUsrc, RegWrite, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_COMP_NEQ; // "out" in ALU is 0 then "zero" flag is 1
-		    SignExtend = 1;
+		            SignExtend = 1;
                 end 
 
                 `BNE: begin 
                     {Branch} = 1'b1;
-                    {ALUsrc, RegWrite, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_COMP_EQ; // "out" in ALU is 0 then "zero" flag is 1
-		    SignExtend = 1;
+		            SignExtend = 1;
                 end 
 
                 `BLEZ: begin
                     {Branch} = 1'b1;
-                    {ALUsrc, RegWrite, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_COMP_GT; // "out" in ALU is 0 then "zero" flag is 1
-		    SignExtend = 1;
+		            SignExtend = 1;
                 end
 
                 `BGTZ: begin
                     {Branch} = 1'b1;
-                    {ALUsrc, RegWrite, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_COMP_LT; // "out" in ALU is 0 then "zero" flag is 1
-		    SignExtend = 1;
+		            SignExtend = 1;
                 end
 
                 `LW: begin
                     {ALUsrc, Link, RegWrite, MemRead, MemToReg} = 5'b11111;
-                    {Jump, RegDest, MemWrite} = 3'b000;
                     {ALUOp} = `ALU_ADD;
-		    SignExtend = 1;
+		            SignExtend = 1;
                 end
 
                 `SW: begin
                     {ALUsrc, MemWrite} = 2'b11;
-                    {Jump, RegWrite, MemRead, RegDest} = 4'b0000;
                     {ALUOp} = `ALU_ADD;
-		    SignExtend = 1;
+		            SignExtend = 1;
                 end
 
                 `SLTi: begin
                     {ALUsrc, Link, RegWrite} = 3'b111;
-                    {RegDest, MemToReg, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_COMP_LE;
-		    SignExtend = 1;
+		            SignExtend = 1;
                 end
 
                 `LUI: begin
                     {ALUsrc, Link, RegWrite} = 3'b111;
-                    {RegDest, MemToReg, MemRead, MemWrite, Jump} = 5'b00000;
                     {ALUOp} = `ALU_LUI;
-		    SignExtend = 1;
+                    SignExtend = 1;
                 end
 		default: begin
             $display("UNKOWN OPCODE: %b", opcode);
