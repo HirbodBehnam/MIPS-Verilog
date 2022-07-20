7c9d6debce5f06aeb2ade2cde0a911991839d8b2
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2022-07-20 11:45:45 +0430

    Fixed more stuff

--- src/mips_core.sv
+++ src/mips_core.sv
@@ -67,73 +67,60 @@ wire cache_ready;
 wire [7:0] cache_data_out [0:3];
 assign mem_data_in = cache_data_out;
 wire [7:0] cache_data_in [0:3];
-assign {cache_data_in[0], cache_data_in[1], cache_data_in[2], cache_data_in[3]} = exmem_readdata2;
+assign {cache_data_in[0], cache_data_in[1], cache_data_in[2], cache_data_in[3]} = exmem_register_data_2;
 wire cache_enable = exmem_memread | exmem_memwrite; // Enable cache if there is a read or write
 wire stall = cache_enable & (~cache_ready); // Stall if we are reading from memory and the data is not ready
 //pipeline registers
 reg[31:0] ifid_instruction;
-reg[31:0] ifid_pcp4 ;
-reg[31:0] idex_readdata ;
+reg[31:0] ifid_pcp4;
+reg[31:0] idex_register_data_1;
+reg[31:0] idex_register_data_2;
 reg[4:0] idex_writeregister;
-reg[31:0] idex_readdata2 ;
-reg[31:0] idex_signextend ; 
-reg idex_cjump ;
-reg idex_cjump_reg; // TODO: connect
-reg idex_cbranch ;
-reg idex_cmemtoreg ;
-reg[4:0] idex_aluop ;
-reg idex_regwrite ;
-reg idex_memread ;
-reg idex_membyte ;
-reg idex_memwrite; // TODO: connect
-reg idex_ALU_src; // TODO: connect
-reg[31:0] idex_pc ;
-reg[31:0] idex_instruction ;
-reg[4:0] exmem_writeregister ;
-reg exmem_regwrite ;
-reg exmem_cjump ;
-reg exmem_cjump_reg; // TODO: connect
-reg exmem_cbranch ;
-reg exmem_memtoreg ;
-reg[31:0] exmem_pc ;
-reg exmem_memread ;
-reg exmem_membyte ;
-reg exmem_memwrite; // TODO: connect
-reg exmem_zero ;
-reg[31:0] exmem_readdata2 ; // TODO: connect
-reg[31:0] exmem_alu_result ;
-reg[31:0] exmem_instruction;
-reg[31:0] exmem_immediate;
-reg[4:0] memwb_writeregister;
-reg[31:0] memwb_readdata ;
-reg[31:0] memwb_pc; // TODO: connect
-reg[31:0] memwb_first_register_data; // TODO: connect
-reg memwb_regwrite ;
-reg memwb_cjump ;
-reg memwb_cbranch ;
-reg memwb_memtoreg ;
-reg memwb_memwrite ;
-reg memwb_memread ;
-reg memwb_membyte ;
-reg[31:0] memwb_alu_result ; // TODO: connect
-reg[4:0] wb_writeregister ;
-reg wb_regwrite ;
-reg wb_memtoreg ;
-reg[31:0] wb_readdata ;
-reg idex_halted ;
-reg exmem_halted;
-reg memwb_halted;
-reg wb_halted;
+reg[31:0] idex_signextend; 
+reg idex_cjump;
+reg idex_cjump_reg;
+reg idex_cbranch;
+reg idex_cmemtoreg;
+reg[4:0] idex_aluop;
+reg idex_regwrite;
+reg idex_memread;
+reg idex_membyte;
+reg idex_memwrite;
+reg idex_ALU_src;
 reg idex_link;
+reg idex_halted;
+reg[31:0] idex_pc;
+reg[31:0] idex_instruction;
+reg[4:0] exmem_writeregister;
+reg exmem_regwrite;
+reg exmem_cjump;
+reg exmem_cjump_reg;
+reg exmem_cbranch;
+reg exmem_memtoreg;
+reg[31:0] exmem_pc;
+reg exmem_memread;
+reg exmem_membyte;
+reg exmem_memwrite;
+reg exmem_zero;
+reg[31:0] exmem_register_data_1;
+reg[31:0] exmem_register_data_2;
+reg[31:0] exmem_alu_result;
+reg[31:0] exmem_instruction;
 reg exmem_link;
+reg exmem_halted;
+reg[4:0] memwb_writeregister;
+reg[31:0] memwb_memory_read_data;
+reg[31:0] memwb_pc;
+reg memwb_regwrite;
+reg memwb_memtoreg;
+reg[31:0] memwb_alu_result;
+reg memwb_halted;
 reg memwb_link;
-reg wb_link;
-reg[31:0] memwb_instruction;
-reg[31:0] wb_instruction;
+
 //instantiation
 ALU al(
 	.opt(idex_aluop),
-	.a(idex_readdata),
+	.a(idex_register_data_1),
 	.b(a_immidate_or_reg_data),
 	.shamt(idex_instruction[10:6]),
 	.out(a_out),
@@ -201,21 +188,21 @@ Cache cache(
 assign r_writereg1 = c_Link ? (c_RegDst ? inst[15:11] : inst[20:16]) : 5'd31; // Done in register file stage
 
 assign r_writedata = memwb_link ?
-					(memwb_memtoreg ? memwb_readdata : memwb_alu_result)
+					(memwb_memtoreg ? memwb_memory_read_data : memwb_alu_result)
 					: memwb_pc; // Done in write back
 
-assign a_immidate_or_reg_data = idex_ALU_src ? idex_signextend : idex_readdata2;
+assign a_immidate_or_reg_data = idex_ALU_src ? idex_signextend : idex_register_data_2;
 
-assign halted = wb_halted;
+assign halted = memwb_halted;
 
 // All of this must be done in mem phase
 assign inst_addr_load = exmem_cjump_reg ? // jr
-						memwb_first_register_data :
+						exmem_register_data_1 :
 						(
-							exmem_cjump ? // j
+							exmem_cjump ? // j and jal
 							{exmem_pc[31:28], exmem_instruction[27:0] << 2} :
 							(
-								(c_Branch & a_z) ? // branch
+								(exmem_cbranch & exmem_zero) ? // branch
 								exmem_pc + (ext_15_0 << 2) :
 								// Note to myself: We are always running this in each block.
 								// So this must get the current pc counter
@@ -237,56 +224,51 @@ always @(posedge clk or negedge rst_b) begin
 			ifid_instruction <= inst;
 			ifid_pcp4 <= inst_addr + 4;
 			// After register file. Includes the registers and control unit signals
-			idex_readdata <= r_read1; // First register output
-			idex_readdata2 <= r_read2; // Second register output
+			idex_register_data_1 <= r_read1; // First register output
+			idex_register_data_2 <= r_read2; // Second register output
 			idex_signextend <= ext_15_0; // Sign extend result
 			idex_cjump <= c_Jump; // Is this jump? Will be needed until Mem
+			idex_cjump_reg <= c_JumpReg; // Enabled on jal instruction. Will be used in mem
 			idex_cbranch <= c_Branch; // Is this branch? Will be needed until Mem
 			idex_cmemtoreg <= c_MemToReg; // Should we write the memory data into registers? Goes until end
 			idex_aluop <= c_ALUOp; // ALU operation. Goes until execute
 			idex_regwrite <= c_regWrite; // Write enable of register file. Goes until very end
 			idex_memread <= c_MemRead; // Should we read from memory? Goes until memory
 			idex_membyte <= c_MemByte; // Is this a byte read? Goes unitl memory
+			idex_memwrite <= c_MemWrite; // Should we write to memory? Goes until memory
+			idex_ALU_src <= c_ALUsrc; // Should ALU use the immidate or the register?
 			idex_pc <= ifid_pcp4; // From last step. The PC + 4
 			idex_writeregister <= r_writereg1; // The register number which data should be written to. Goes until end
 			idex_instruction <= ifid_instruction; // The instruction itself
+			idex_halted <= c_halted; // We must halt after we have written to registers
+			idex_link <= c_Link; // True if we want to jal. Goes until memory
 			// After Execute (ALU)
-			exmem_regwrite <= idex_regwrite;
-			exmem_cjump <= idex_cjump;
-			exmem_cbranch <= idex_cbranch;
-			exmem_memtoreg <= idex_cmemtoreg;
-			exmem_pc <= idex_pc;
-			exmem_memread <= idex_memread;
-			exmem_membyte <= idex_membyte;
- 			exmem_zero <= a_z;
-			exmem_alu_result <= a_out;
-			exmem_immediate <= idex_readdata2;
-			exmem_writeregister <= idex_writeregister;
-			exmem_instruction <= idex_instruction;
-			memwb_readdata <= {cache_data_out[0], cache_data_out[1], cache_data_out[2], cache_data_out[3]};
-			memwb_regwrite <= exmem_regwrite;
-			memwb_cjump <= exmem_cjump;
-			memwb_cbranch <= exmem_cbranch;
-			memwb_memtoreg <= exmem_cmemtoreg;
-			memwb_memwrite <= c_MemWrite;
-			memwb_memread <= exmem_memread;
-			memwb_membyte <= exmem_membyte;
-			memwb_writeregister <= exmem_writeregister;
-			wb_regwrite <= memwb_regwrite;
-			wb_memtoreg <= memwb_memtoreg;
-			wb_readdata <= memwb_readdata;
-			wb_writeregister <= memwb_writeregister;
-			idex_halted <= c_halted;
-			exmem_halted <= idex_halted;
-			memwb_halted <= exmem_halted;
-			wb_halted <= memwb_halted;
-			idex_link <= c_Link;
-			exmem_link <= idex_link;
-			memwb_link <= exmem_link;
-			wb_link <= memwb_link;
-			exmem_instruction <= idex_instruction;
-			memwb_instruction <= exmem_instruction;
-			wb_instruction <= memwb_instruction;
+			exmem_regwrite <= idex_regwrite; // The write enable which must go until end
+			exmem_cjump <= idex_cjump; // Should we jump? This is the last one
+			exmem_cbranch <= idex_cbranch; // Should we branch if needed? This is the last step
+			exmem_memtoreg <= idex_cmemtoreg; // Should we write memory into registers? Goes until next stage
+			exmem_cjump_reg <= idex_cjump_reg; // Should we jump to register? (jr)
+			exmem_pc <= idex_pc; // The program counter of the command which is being executed
+			exmem_memread <= idex_memread; // Should we read from memory?
+			exmem_membyte <= idex_membyte; // Should we read/write byte?
+			exmem_memwrite <= idex_memwrite; // Should we write to memory?
+			exmem_register_data_1 <= idex_register_data_1; // Needed for jr in next 
+			exmem_register_data_2 <= idex_register_data_2; // Needed for data to be written to memory
+ 			exmem_zero <= a_z; // Is the alu result zero?
+			exmem_alu_result <= a_out; // The result of alu
+			exmem_link <= idex_link; // Link used in jal
+			exmem_writeregister <= idex_writeregister; // Number of register to write in
+			exmem_instruction <= idex_instruction; // The instruction itself
+			exmem_halted <= idex_halted; // Halt at last
+			// Memory phase (last one)
+			memwb_memory_read_data <= {cache_data_out[0], cache_data_out[1], cache_data_out[2], cache_data_out[3]}; // Mem out
+			memwb_regwrite <= exmem_regwrite; // Write enable of register file
+			memwb_writeregister <= exmem_writeregister; // The register to write into
+			memwb_alu_result <= exmem_alu_result; // The alu result if this is write back
+			memwb_pc <= exmem_pc; // The program counter because we might write back to register
+			memwb_memtoreg <= exmem_memtoreg; // Should memory data go to register or alu
+			memwb_halted <= exmem_halted; // Halt the system if needed
+			memwb_link <= exmem_link; // If link is true the pc will go to register
 		end
 	end
 end
