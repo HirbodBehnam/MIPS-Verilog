7b2d4ec7694ca3428fb008c9cdf8e963887f7bae
Beny-Maleki <benyaminmaleki81@gmail.com>
2022-07-24 11:48:37 +0430

    A small change in FP-Div

---
+++ src/floating_point/fp_div.sv
@@ -0,0 +1,11 @@
+`include "fp_consts.sv";
+
+module FP_Divider (
+    input [31:0] a,
+    input [31:0] b,
+    output reg [31:0] result,
+    output reg overflow,
+    output reg underflow
+);
+    
+endmodule
